f39d636e2968af17c787cc7cfd0454a04531042f
bdtomlin <bdtomlin@gmail.com>
2010-07-17 16:47:42 -0500

    added 960.js

---
+++ public/javascripts/init.js
@@ -0,0 +1,23 @@
+var gOverride = {
+  gColor: '#EEEEEE',
+  gColumns: 12,
+  gOpacity: 0.35,
+  gWidth: 10,
+  pColor: '#000',
+  pHeight: 21,
+  pOffset: 0,
+  pOpacity: 0.55,
+  center: true,
+  gEnabled: false,
+  pEnabled: true,
+  setupEnabled: true,
+  fixFlash: true,
+  size: 960
+};
+
+createGridder = function() {
+  document.body.appendChild(document.createElement('script')).src='/960gridder/960.js';
+};
+window.onload=function(){
+ createGridder();
+};
